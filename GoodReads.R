5122de349eb67b363300422c228f7fa7e5773722
xiomaradv <xdominguez@smith.edu>
2024-04-18 19:39:20 -0400

    Cleaned the titles.

--- GoodReads.R
+++ GoodReads.R
@@ -2,7 +2,7 @@ library(rvest)
 goodreads <- rvest::read_html("https://www.goodreads.com/list/show/1.Best_Books_Ever")
 
 Df_title <- goodreads |>
-  html_elements("div h1") |>
+  html_elements(".gr-h1--serif") |>
   html_text2()
 Df_title
 
@@ -25,11 +25,6 @@ rating_chunk <- goodreads|>
   html_elements(".minirating") |>
   html_text2()
 rating_chunk
-
-score_chunk <- goodreads|>
-  html_elements(".uitext a:nth-child(1)") |>
-  html_text2()
-score_chuck
 
 cover <- goodreads|>
   html_elements(".bookCover") |>
