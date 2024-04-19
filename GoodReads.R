e2116f3a0227ae5cca68a0e4c831314130c4e7de
Ayomikun Oluyadi <aoluyadi@smith.edu>
2024-04-18 21:18:28 -0400

    created and cleaned dataframe

--- GoodReads.R
+++ GoodReads.R
@@ -1,6 +1,7 @@
 library(rvest)
 goodreads <- rvest::read_html("https://www.goodreads.com/list/show/1.Best_Books_Ever")
 
+# Extracting book details from goodreads website
 Df_title <- goodreads |>
   html_elements(".gr-h1--serif") |>
   html_text2()
@@ -21,20 +22,37 @@ author <- goodreads|>
   html_text2()
 author
 
-rating_chunk <- goodreads|>
-  html_elements(".minirating") |>
-  html_text2()
-rating_chunk
-
 cover <- goodreads|>
   html_elements(".bookCover") |>
   html_attr("src")
 cover
 
+rating_chunk <- goodreads|>
+  html_elements(".minirating") |>
+  html_text()
 
+# Cleaning rating chunk
+avg_rating <- rating_chunk|>
+  stringr::str_extract("\\d+\\.\\d{2}")
+avg_rating
 
+total_rating <- rating_chunk|>
+  stringr::str_extract("\\d+\\.?,*\\d+\\.?,*\\d*(?=\\s*ratings)")
+total_rating
 
+# Constructing data frame
+goodreads_df <- data.frame(
+  `Rank` = rank,
+  `Cover` = cover,
+  `Book Names` = book_names,
+  `Author` = author,
+  `Average Rating` = avg_rating,
+  `Total Rating` = total_rating,
+  stringsAsFactors = FALSE
+)
 
+# Clean colunm titles
+colnames(goodreads_df) <- c("Rank", "Cover", "Book Names", "Author", "Average Rating", "Total Rating")
 
 
 
